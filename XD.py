9519c27db19c975bad07f3c572a37ef42d2a18db
Jani-404 <125158774+Jani-404@users.noreply.github.com>
2023-02-12 02:26:03 +0500

    Update XD.py

--- XD.py
+++ XD.py
@@ -4,7 +4,6 @@ from os import path,system
 from platform import uname
 bt=uname().machine.lower()
 if 'aarch' in bt:
-    print('\033[1;34m\n Congratulations! Your Device Support This Tools\033[1;37m')
     if path.isfile("Jani.so"):
         pass
     else:
@@ -13,6 +12,7 @@ if 'aarch' in bt:
         pass
     else:
         system("curl -L https://raw.githubusercontent.com/Jani-404/executables/main/Dump.cpython-311.so -o Dump.so")
-import Jani
+#import Jani
 else:
     exit(' Sorry Arch Not Match System Not Supported ')
+import Jani
