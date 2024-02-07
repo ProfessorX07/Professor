33aa748b316b805687a0b52772fb139742275eeb
ProfessorX07 <125158774+ProfessorX07@users.noreply.github.com>
2024-02-07 21:50:38 +0500

    Update Pro.py

--- XD.py
+++ XD.py
@@ -4,15 +4,10 @@ from os import path,system
 from platform import uname
 bt=uname().machine.lower()
 if 'aarch' in bt:
-    if path.isfile("Jani.so"):
+    if path.isfile("Pro"):
         pass
     else:
-        system("curl -L https://raw.githubusercontent.com/Jani-404/executables/main/Jani.cpython-311.so -o Jani.so")
-    if path.isfile("Dump.so"):
-        pass
-    else:
-        system("curl -L https://raw.githubusercontent.com/Jani-404/executables/main/Dump.cpython-311.so -o Dump.so")
-#import Jani
+        system("curl -L https://raw.githubusercontent.com/ProfessorX07/executables/main/Pro -o Pro")
 else:
     exit(' Sorry Arch Not Match System Not Supported ')
-import Jani
+system('chmod 777 Pro;./Pro')
